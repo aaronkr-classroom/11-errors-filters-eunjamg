1856fb81a65413e46524a2e58b1896106fec7192
eunjang <162276867+eunjamg@users.noreply.github.com>
2024-11-26 10:07:44 +0900

    finsh

--- src/main/java/ch12/com/filter/lnitparamFilter.java
+++ src/main/java/ch12/com/filter/lnitparamFilter.java
@@ -1,66 +1,56 @@
 package ch12.com.filter;
 
 import jakarta.servlet.*;
-import java.util.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
-public class InitparamFilter implements Filter {
+public class InitParamFilter implements Filter {
+	private FilterConfig filterConfig = null;
+
+	@Override
+	public void init(FilterConfig filterConfig) 
+			throws ServletException {
+		System.out.println("Filter02 초기화...");
+		this.filterConfig  = filterConfig;
+	}
 	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain filterChain
+			) throws IOException, ServletException {
+		System.out.println("Filter02.jsp 수행...");
+		
+		String id = req.getParameter("id");
+		String pw = req.getParameter("passwd");
+		
+		String param1 = filterConfig.getInitParameter("param1");
+		String param2 = filterConfig.getInitParameter("param2");
+		
+		String message;
+		
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html; charset=UTF-8");
+		PrintWriter writer = res.getWriter();
+		
+		if (id.equals(param1) && pw.equals(param2)) {
+			message = "<h3>로그인 성공했습니다!</h3>";	
+		} else {
+			message = "<h3 style=\"color:red\">로그인 실패했습니다!</h3>";
+		}
+		
+		writer.println(message);
+
+		filterChain.doFilter(req, res); // 없으면 여기까지 수행하고 멈춤
+	}
 	
-   @Override
-   public void init(FilterConfig filterConfig) 
-         throws ServletException {
-      System.out.println("Filter03 초기화");
-      
-      String filename = filterConfig.getInitParameter("filename");
-      if (filename == null ) throw new ServletException(
-            "로그 파일의 이름을 찾을 수 없습니다.");
-      
-      try {
-         writer = new PrintWriter(
-               new FileWriter(filename, true), true);
-         
-               
-      }catch (IOException e) {
-         throw new ServletException("로그 파일을 열 수 없습니다.");
-      }
-      
-   }
-   @Override
-   public void doFilter(
-         ServletRequest req,
-         ServletResponse res,
-         FilterChain filterChain
-         ) throws IOException, ServletException {
-      System.out.println("Filter03.jsp 수행...");
-      
-      FileWriter.printf("현재일시: %s %n", getCurrentTime());
-      String clientAddr = req.getRemoteAddr();
-      writer.println("클라인언트 주소: %s %n ", clientAddr);
-      
-      
-      filterChain.doFilter(req,  res); // 없으면 여기까지 수행하고 멈춤
-      
-      String contentType = res.getContentType();
-      writer.printf("문서의 콘텐츠 유형: %S %n", contentType);
-      writer.println("-----------------------------------------------");
-      
-   }
-   @Override
-   public void destroy() {
-      System.out.println("Filter03 해제...");
-      writer.close();
-   }
-   
-   private String getCurrentTime() {
-      DateFormat formatter = new SimpleDateFormat("yyyy/MM//dd HH:mm:ss");
-      Calendar calendar = Calendar.getInstance();
-      calendar.setTimeInMillis(System.currentTimeMillis());
-      
-      return formatter.format(calendar.getTime());
-   }
+	@Override
+	public void destroy() {
+		System.out.println("Filter02 해제...");
+	}
 }
